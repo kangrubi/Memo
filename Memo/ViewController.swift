c28b34ea7827589f6ef7ca3bb8ca6cbd9bb6f067
rubi kang <rubi0624@nate.com>
2022-01-11 15:04:38 +0900

    Initial Commit

---
+++ Memo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Memo
+//
+//  Created by 강루비 on 2022/01/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
