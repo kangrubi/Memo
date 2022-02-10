2767fc8aef0ffd1c0b61c946bee017b4dfba5819
rubi kang <rubi0624@nate.com>
2022-02-10 18:36:02 +0900

    step 2

--- Memo/ViewController.swift
+++ Memo/ViewController.swift
@@ -12,10 +12,17 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     @IBOutlet weak var tableView: UITableView!
     
     // MARK : Propertes
-    // 제목 정보가 담긴 배열
-    let memoTitleArray: [String] = ["장보기", "장보기2", "장보기3", "장보기4"]
-    // 내용 정보가 담긴 배열
-    let memoContentArray: [String] = ["피자, 햄버거, 주스", "고기, 만두, 샐러드", "김밥, 우동, 떡볶이", "튀김, 어묵, 햄"]
+    struct Memo {
+        var title: String
+        var content: String
+    }
+    
+    var memoList: [Memo] = [
+        Memo(title: "장보기", content: "피자, 햄버거, 주스"),
+        Memo(title: "장보기2", content: "고기, 만두, 샐러드"),
+        Memo(title: "장보기3", content: "김밥, 우동, 떡볶이"),
+        Memo(title: "장보기4", content: "튀김, 어묵, 햄")
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,18 +34,15 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     // MARK: UITableViewSource
     // numberOfRowsInSection - 섹션(section) 안에 있는 행(row)들 갯수
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return memoTitleArray.count
+        return memoList.count
     }
     
     // cell for row at - 그 줄에 어떤 셀을 보여줄 것인지, 그 셀에 대해 정의
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "memoCell", for: indexPath) as? MemoTableViewCell else { return UITableViewCell() }
         
-        let title = memoTitleArray[indexPath.row]
-        let content = memoContentArray[indexPath.row]
-        
-        cell.titleLabel.text = title
-        cell.contantLabel.text = content
+        cell.titleLabel.text = memoList[indexPath.row].title
+        cell.contantLabel.text = memoList[indexPath.row].content
         
         return cell
     }
@@ -48,7 +52,5 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 90
     }
-    
-
 }
 
